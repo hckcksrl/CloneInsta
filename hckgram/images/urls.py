7dd5e8bdd015ac7d5fcfaf09d60caecda9ccd3e5
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-06-25 19:56:09 +0900

    likes api setting

--- hckgram/images/urls.py
+++ hckgram/images/urls.py
@@ -4,21 +4,31 @@ from . import views
 app_name = "images"
 
 urlpatterns = [
+    # path(
+    #     "all/",
+    #     view = views.ListAllImages.as_view(),
+    #     name = 'all_images'
+    # ),
+    # path(
+    #     "comments/",
+    #     view = views.ListAllComments.as_view(),
+    #     name = 'all_comments'
+    # ),
+    # path(
+    #     "likes/",
+    #     view = views.ListAllLikes.as_view(),
+    #     name = 'all_likes'
+    # )
     path(
-        "all/",
-        view = views.ListAllImages.as_view(),
-        name = 'all_images'
+        "",
+        view = views.Feed.as_view(),
+        name = 'feed'
     ),
     path(
-        "comments/",
-        view = views.ListAllComments.as_view(),
-        name = 'all_comments'
+        "<int:image_id>/like", 
+        view = views.LikeImage.as_view(),
+        name = 'like_image'
     ),
-    path(
-        "likes/",
-        view = views.ListAllLikes.as_view(),
-        name = 'all_likes'
-    )
 
 
 ]
