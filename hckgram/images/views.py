a5575e24c0a65f2b791ed522f8389c8287592001
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-06-28 02:03:23 +0900

    Views class Add

--- hckgram/images/views.py
+++ hckgram/images/views.py
@@ -63,7 +63,7 @@ class Feed(APIView):
 
 class LikeImage(APIView):
 
-    def get(self , request ,image_id ,format=None):
+    def post(self , request ,image_id ,format=None):
 
         user = request.user
 
@@ -76,8 +76,7 @@ class LikeImage(APIView):
                 creator = user,
                 image = found_image                
             )
-            preexist_like.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response(status=status.HTTP_304_NOT_MODIFIED)
         except models.Like.DoesNotExist:
             new_like = models.Like.objects.create(
                 creator = user,
@@ -85,3 +84,82 @@ class LikeImage(APIView):
             )
             new_like.save()
             return Response(status=status.HTTP_200_OK)
+
+
+class UnLikeImage(APIView):
+
+    def delete(self,request,image_id,format=None):
+
+        user = request.user
+
+        try:
+            found_image = models.Image.objects.get(id = image_id)
+        except models.Image.DoesNotExist:
+            return Response(status = status.HTTP_404_NOT_FOUND)
+        try:
+            exist_like = models.Like.objects.get(
+                creator= user,
+                image = found_image
+            )
+            exist_like.delete()
+            return Response(status =status.HTTP_204_NO_CONTENT)
+
+        except models.Like.DoesNotExist:
+            return Response(status=status.HTTP_304_NOT_MODIFIED)
+
+
+
+class CommentOnImage(APIView):
+
+    def post(self,request,image_id,format=None):
+
+        user = request.user
+
+        try:
+            found_image = models.Image.objects.get(id = image_id)   # comment 달려고하는 image가 있는지 확인
+        except models.Image.DoesNotExist :
+            return Response(status =status.HTTP_404_NOT_FOUND)
+
+        serializer = serializers.CommentSerializer(data = request.data) #  data를 serializer한다.(CommentSerializer)형태
+
+        if serializer.is_valid() :  #   serializer 가 있으면
+            serializer.save(creator=user, image = found_image)  #   serializer save를한다. creator 는 user , image 는 found_image
+            return Response(data = serializer.data , status=status.HTTP_201_CREATED)
+        else : 
+            return Response(data = serializer.errors,status = status.HTTP_400_BAD_REQUEST)
+
+class DeleteComment(APIView):
+
+    def delete(self,request,comment_id,format =None):
+        
+        user = request.user
+
+        try:
+            found_comment = models.Comment.objects.get(id = comment_id , creator = user)    
+            #   삭제할려는 comment의  comment_id를 찾고    찾은 comment의 creator 가 user와 같으면
+            found_comment.delete()  #   found_comment 를 삭제
+            return Response(status=status.HTTP_200_OK)
+        except models.Comment.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+
+class Search(APIView):
+
+    def get(self , request, format=None):
+
+        hashtags = request.query_params.get('hashtags',None)
+        
+        if hashtags is not None:
+            hashtags = hashtags.split(",")
+            
+            images = models.Image.objects.filter(
+                tags__name__in = hashtags).distinct() 
+            #   tags__name__in 은  Deep Relationship이다
+            #   A__B__contains,exact 는 객체안의 A객체 안의 B의 값이 contains, exact 일떄 대소문자 구별 은 앞에 i를 붙이면됨
+            
+            serializer = serializers.CountImageSerializer(images , many=True)
+
+            return Response(data = serializer.data , status = status.HTTP_200_OK)
+
+        else :
+            return Response(status = status.HTTP_400_BAD_REQUEST)
