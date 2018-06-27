46f166d7827c47b8785573658e83a41e29668288
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-06-28 02:00:57 +0900

    Add notifications

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,10 @@ urlpatterns = [
         "images/",
         include("hckgram.images.urls", namespace="images"),
     ),
+    path(
+        "notifications/",
+        include("hckgram.notifications.urls", namespace="notifications"),
+    ),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
 ] + static(
