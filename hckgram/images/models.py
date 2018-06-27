99ac4f231277ca836e2c024427c7172c3a52e594
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-06-28 02:01:59 +0900

    Add Tags and Count_comments

--- hckgram/images/models.py
+++ hckgram/images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from taggit.managers import TaggableManager
 from hckgram.users import models as user_models
 # Create your models here.
 class TimeStampModel(models.Model):
@@ -18,10 +19,16 @@ class Image(TimeStampModel):
     caption = models.TextField()
     creator = models.ForeignKey(user_models.User,
         on_delete = models.CASCADE, null=True, related_name ='images')
+    tags = TaggableManager()
+
 
     @property   # 데이터로 가지는 않지만 모델안에 존재 , function 이다
     def count_likes(self):
         return self.likes.all().count() #count()는 숫자를 세는 python함수
+
+    @property
+    def count_comments(self):
+        return self.comments.all().count()
 
     def __str__(self):
         return '{} - {}'.format(self.location,self.caption)
