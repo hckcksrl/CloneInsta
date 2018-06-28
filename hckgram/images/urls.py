667e78105afebdf0cce6fd2f362848b862a7423e
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-06-28 20:25:38 +0900

    images rest api

--- hckgram/images/urls.py
+++ hckgram/images/urls.py
@@ -4,33 +4,23 @@ from . import views
 app_name = "images"
 
 urlpatterns = [
-    # path(
-    #     "all/",
-    #     view = views.ListAllImages.as_view(),
-    #     name = 'all_images'
-    # ),
-    # path(
-    #     "comments/",
-    #     view = views.ListAllComments.as_view(),
-    #     name = 'all_comments'
-    # ),
-    # path(
-    #     "likes/",
-    #     view = views.ListAllLikes.as_view(),
-    #     name = 'all_likes'
-    # )
     path(
         "",
-        view = views.Feed.as_view(),
+        view = views.Images.as_view(),
         name = 'feed'
     ),
     path(
-        "<int:image_id>/like", 
+        "<int:image_id>",
+        view = views.ImageDetail.as_view(),
+        name = 'image_detail'
+    ),
+    path(
+        "<int:image_id>/likes", 
         view = views.LikeImage.as_view(),
         name = 'like_image'
     ),
     path(
-        "<int:image_id>/unlike", 
+        "<int:image_id>/unlikes", 
         view = views.UnLikeImage.as_view(),
         name = 'unlike_image'
     ),
@@ -48,6 +38,11 @@ urlpatterns = [
         "search/",
         view = views.Search.as_view(),
         name='search'
+    ),
+    path(
+        "<int:image_id>/comments/<int:comment_id>",
+        view = views.ImageComment.as_view(),
+        name = 'image_comment_delete'
     )
 
 ]
